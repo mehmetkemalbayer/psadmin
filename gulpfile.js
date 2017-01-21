dc6a6a14e7bc47c14f6388e3d6ffd3272e87c97d
softtech\kemal.bayer <kemal.bayer@softtech.com.tr>
2017-01-21 19:20:52 +0300

    changes until end of module react router and demo bootstrapping

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        images: './src/images/*',
         css: [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -77,4 +78,14 @@ gulp.task('watch', function() {
     gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('images', function() {
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images') )
+        .pipe(connect.reload());
+    gulp.src('./src/favicon.ico')
+        .pipe(gulp.dest(config.paths.dist));
+});
+
+
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch', 'images']);
